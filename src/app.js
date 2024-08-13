99019edfb14147120f358f55addfc095abd43565
minji <minji5578@gmail.com>
2024-08-13 22:32:21 +0900

    mix App.js and app.js

--- src/app.js
+++ src/app.js
@@ -1,9 +1,31 @@
+// Front-End
+import React from 'react';
+
+import GroupForm from './components/GroupForm';
+import Layout from './components/Layout';  // Layout 컴포넌트를 import
+
+// Back-End
 import express from 'express';
 
 import groupController from './controllers/groupController.js';
-import postController from './controllers/postController.js';
+import postController from './controllers/postController.js'
 
 
+// ***************************************************************
+// Front-End
+function App() {
+  return (
+    <div className="App">
+      <Layout>
+        <GroupForm />
+      </Layout>
+    </div>
+  );
+}
+
+
+// ***************************************************************
+// Back-End
 const app = express();
 app.use(express.json());
 
@@ -16,3 +38,7 @@ const port = process.env.PORT ?? 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+
+// ***************************************************************
+export default App;
