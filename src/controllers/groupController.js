6d92694896252157dbed6da305bc5522414c24ff
minji <minji5578@gmail.com>
2024-08-13 15:28:01 +0900

    Make file : config/prisma.js

--- src/controllers/groupController.js
+++ src/controllers/groupController.js
@@ -2,4 +2,9 @@ import express from 'express';
 
 const groupController = express.Router();
 
+productController.post('/', async (req, res, next) => {
+  const createGroup = await groupService.create(req.body);
+  return res.json(createGroup);
+});
+
 export default productController;
