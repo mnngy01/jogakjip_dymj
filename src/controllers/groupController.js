055628dfac6d8c162acdf49e8dffd874060398b8
minji <minji5578@gmail.com>
2024-08-15 18:09:18 +0900

    Delete schema.prisma and Modify groupController.js

--- src/controllers/groupController.js
+++ src/controllers/groupController.js
@@ -4,6 +4,7 @@ import groupService from '../services/groupService.js';
 
 const groupController = express.Router();
 
+// 그룹 등록하기
 productController.post('/', async (req, res, next) => {
   const createGroup = await groupService.create(req.body);
   return res.json(createGroup);
