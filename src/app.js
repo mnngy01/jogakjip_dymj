641ee0e2c0b20c13443b902980b69c571e85ea24
minji <minji5578@gmail.com>
2024-09-10 22:17:38 +0900

    Modify for deploy

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import routes from './routes.js';
 import path from 'path';
+import cors from 'cors';
 
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
 // 전송된 파일을 담기 위한 폴더
