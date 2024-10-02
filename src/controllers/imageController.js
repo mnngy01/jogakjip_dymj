cc7b617be037842d8a160459dbf42e239bfdd654
minji <minji5578@gmail.com>
2024-10-02 19:30:16 +0900

    Modify imageController.js

--- src/controllers/imageController.js
+++ src/controllers/imageController.js
@@ -3,9 +3,13 @@ import multer from 'multer';
 const upload = multer({ dest: 'uploads/' });
 
 const imageUpload = async (req, res) => {
-  const filename = req.file.filename;
-  const path = `/profile${filename}`;
-  res.json({ path });
+  try{
+    const filename = req.file.filename;
+    const path = `/profile${filename}`;
+    res.jstatus(200).json({ imageUrl: path });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 };
 
 export default {
