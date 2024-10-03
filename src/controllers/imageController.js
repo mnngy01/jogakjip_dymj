af04cd53af72af19b9f00049b18a91f8db500e9f
minji <minji5578@gmail.com>
2024-10-03 15:24:07 +0900

    Modify type error in imageController.js

--- src/controllers/imageController.js
+++ src/controllers/imageController.js
@@ -6,7 +6,7 @@ const imageUpload = async (req, res) => {
   try{
     const filename = req.file.filename;
     const path = `/profile${filename}`;
-    res.jstatus(200).json({ imageUrl: path });
+    res.status(200).json({ imageUrl: path });
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
