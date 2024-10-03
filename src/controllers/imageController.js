d00e8096d71c8427f23284564dbfff2c70a0b50e
minji <minji5578@gmail.com>
2024-10-03 15:58:02 +0900

    Modify path in imageController.js

--- src/controllers/imageController.js
+++ src/controllers/imageController.js
@@ -5,7 +5,7 @@ const upload = multer({ dest: 'uploads/' });
 const imageUpload = async (req, res) => {
   try{
     const filename = req.file.filename;
-    const path = `/profile${filename}`;
+    const path = `/uploads/${filename}`;
     res.status(200).json({ imageUrl: path });
   } catch (error) {
     res.status(400).json({ message: error.message });
